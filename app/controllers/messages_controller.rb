7c5906a3e9342a87f85a2fcb660ca79af3ebf05a
fujifuji123412 <sena.04091919@gmail.com>
2020-12-28 15:51:26 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,22 @@
 class MessagesController < ApplicationController
+
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    if @message.save 
+    if @message.save
       redirect_to room_messages_path(@room)
     else
-      rebder :index
+      @messages = @room.messages.includes(:user)
+      render :index
     end
   end
-  
+
   private
 
   def message_params
